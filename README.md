a77018f5a12efdf444efbf0c3b02fe06eb1635f6
David RJ <david.rud135@gmail.com>
2017-08-30 00:00:21 +0300

    New file

---
+++ README.md
@@ -0,0 +1,22 @@
+# Arithmetic Mean (HTML Version)
+
+My program calculate arithmetic mean of given numbers.
+
+Write numbers you need separating them by "space" and click Calculate or hit Enter.
+
+## How to start
+There are 2 ways:
+ ### 1. Clone repozitory
+Write in Terminal:
+```
+git init
+git clone https://github.com/davidrud135/Arithmetic_Mean.git
+```
+Then go to cloned repository and open index.html file.
+
+ ### 2. Download ZIP archive
+Do as below:
+
+![Image alt](https://github.com/davidrud135/Arithmetic-Mean_HTML-version/blob/develop/zip.png)
+
+Later unpack archive and open index.html file.
---
+++ js/script.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+
+	$('form').submit(function calculateNums() {
+		var dirtyUserInput, cleanUserInput, arrayOfStrings, arrayOfNums,
+			quantityOfNums, sumOfNums, result;
+
+		dirtyUserInput = getInputData();
+
+		cleanUserInput = cleanUnnecessarySpaces(dirtyUserInput);
+
+		if (checkForEmptyField(cleanUserInput)) { return alert('Field should be filled up!') };
+
+		arrayOfStrings = createArrayOfStrings(cleanUserInput);
+
+		arrayOfNums = makeArrayOfNums(arrayOfStrings);
+
+		quantityOfNums = arrayOfNums.length;	
+
+		sumOfNums = sumNums(arrayOfNums, quantityOfNums);
+
+		result = sumOfNums / quantityOfNums;
+
+		showResult(result);
+
+		return false;
+	});
+
+	function getInputData() {
+	return $('input[type=text]').val();
+	}
+
+	function cleanUnnecessarySpaces(userString) {
+		return userString.trim();
+	}
+
+	function checkForEmptyField(userString) {
+		if (userString === '') { return true };
+	}
+
+	function createArrayOfStrings(userString) {
+		return userString.split(' ');
+	}
+
+	function makeArrayOfNums(array) {
+		return array.map(function(item) {return Number(item)} );
+	}
+
+	function sumNums(array, end) {
+		var sum = 0;
+		for(var i = 0; i < end; i++) {
+			if( isNaN(array[i]) ) { alert('Please, write only numbers!') };
+			sum = sum + array[i];
+		}
+
+		return sum;
+	}
+
+	function showResult(result) {
+		$('#result').html('Result: ' + result);
+	}
+});
